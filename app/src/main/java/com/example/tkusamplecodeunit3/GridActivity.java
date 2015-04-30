0587d738ab1cdd1bd975e13ce91dfae26e0eb766
xavier0507 <xavier0507@livemail.tw>
2015-04-30 12:38:05 +0800

    Add initial project
    Signed-off-by: xavier0507 <xavier0507@livemail.tw>

---
+++ app/src/main/java/com/example/tkusamplecodeunit3/GridActivity.java
@@ -0,0 +1,15 @@
+package com.example.tkusamplecodeunit3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Xavier on 2015/4/29.
+ */
+public class GridActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.layout_activity_grid);
+	}
+}
---
+++ app/src/main/java/com/example/tkusamplecodeunit3/HorizLinearActivity.java
@@ -0,0 +1,15 @@
+package com.example.tkusamplecodeunit3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Xavier on 2015/4/28.
+ */
+public class HorizLinearActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.layout_activity_horiz_linear);
+	}
+}
